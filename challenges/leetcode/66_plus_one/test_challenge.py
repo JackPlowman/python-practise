2a9504f94ba78e82a0509e739a630bc1a6b6b912
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-09-22 17:17:26 +0100

    Add LeetCode "Plus One" Challenge (#39)
    # Description
    
    This change adds the solution for LeetCode challenge 66: Plus One.

---
+++ challenges/leetcode/66_plus_one/test_challenge.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import pytest
+
+from challenge import Solution
+
+
+@pytest.mark.parametrize(
+    ("function_input", "expected_output"),
+    [
+        ([1, 2, 3], [1, 2, 4]),
+        ([4, 3, 2, 1], [4, 3, 2, 2]),
+        ([9], [1, 0]),
+        ([9, 9], [1, 0, 0]),
+        ([9, 9, 9, 9, 9], [1, 0, 0, 0, 0, 0]),
+    ],
+)
+def test_solution__plus_one(function_input: list[int], expected_output: list[int]) -> None:
+    # Act
+    output = Solution().plusOne(function_input)
+    # Assert
+    assert expected_output == output
